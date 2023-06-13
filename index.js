ff227fade4780205829515e4ef7a0f4f64c15dde
HongAn <tranhongan12a6@gmail.com>
2023-06-13 11:26:11 +0700

    tesst

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
+import Puppeteer from "puppeteer";
+import * as download from "image-downloader";
 
-
-import  Puppeteer  from 'puppeteer';
-import * as download from 'image-downloader'
-
-(async() => {
+(async () => {
   try {
     const browser = await Puppeteer.launch({
-        headless: "new"
+      headless: "new", //
     });
-    console.log('Browser openned');
+    console.log("Browser openned");
     const page = await browser.newPage();
-    const url = 'https://tannhatminh.com/ky-niem-chuong-pha-le-dep/'
+    const url = "https://tannhatminh.com/ky-niem-chuong-pha-le-dep/";
     await page.goto(url);
-    console.log('Page loaded');
+    console.log("Page loaded");
 
-   const imageLinks =  await page.evaluate(() => {
-        let imgElements = document.querySelectorAll('.product-thumb img');
-        console.log(imgElements)
-        imgElements = [...imgElements];
-        let imgLinks = imgElements.map(i => i.getAttribute('src'));
-        return imgLinks;
+    const imageLinks = await page.evaluate(() => {
+      let imgElements = document.querySelectorAll(".product-thumb img");
+      console.log(imgElements);
+      imgElements = [...imgElements];
+      let imgLinks = imgElements.map((i) => i.getAttribute("src"));
+      return imgLinks;
     });
 
-
-    await Promise.all(imageLinks.map(imgUrl => download.image({
-        url: imgUrl,
-        dest: './kyniemchuongphale'
-    })));
+    await Promise.all(
+      imageLinks.map((imgUrl) =>
+        download.image({
+          url: imgUrl,
+          dest: "./kyniemchuongphale",
+        })
+      )
+    );
 
     await browser.close();
   } catch (error) {
-    console.log('error' ,error)
+    console.log("error", error);
   }
 })();
-
-
-
-
-
-
-
-
-
-
-
